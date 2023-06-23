dad81b73bfe4ed6c6cf4ae8489910b10160ef98f
정승일 <jungsi1217@naver.com>
2023-06-23 18:47:08 +0900

    2023-06-23

---
+++ javascript/039._DOM.js
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+const btn = document.createElement('button')
+btn.innerText = '눌럿!'
+const body = document.querySelector("body")
+body.append(btn)
+
+btn.addEventListener('click', (event) => {
+    console.log(event);
+=======
+const btn = document.createElement('button')
+btn.innerText = '눌럿!'
+const body = document.querySelector("body")
+body.append(btn)
+
+btn.addEventListener('click', (event) => {
+    console.log(event);
+>>>>>>> 90cbed311723ee62013ba46f8a49fb74a300916f
+});
